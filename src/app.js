2a6befa86b417caf974a4a4484d9598a4569c1f4
Demian <demian.sotomayor.ur@gmail.com>
2023-09-21 22:37:52 -0300

    Project excuses generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let quien = ['El perro', 'Mi abuela', 'Su tortuga', 'Mi pájaro'];
+let accion = ['se comió', 'orinó', 'aplastó', 'rompió'];
+let que = ['mi tarea', 'las llaves', 'el auto'];
+let cuando = ['antes de la clase', 'esta mañana', 'cuando terminé', 'mientras almorzaba','mientras rezaba'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+
+function generadoraExcusas(quien, accion, que, cuando) {
+
+  const randomQuien = quien[Math.floor(Math.random() * quien.length)];
+  const randomAccion = accion[Math.floor(Math.random() * accion.length)];
+  const randomQue = que[Math.floor(Math.random() * que.length)];
+  const randomCuando = cuando[Math.floor(Math.random() * cuando.length)];
+  return `${randomQuien} ${randomAccion} ${randomQue} ${randomCuando}`;
+
+}
+
+// Mostrar excusa en el HTML
+function onLoad() {
+
+  const excusa = generadoraExcusas(quien, accion, que, cuando);
+  document.getElementById('excusa').innerHTML = excusa;
+
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  onLoad();
+}
